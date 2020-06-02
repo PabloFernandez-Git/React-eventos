479388f6f22febc7e5bfee03a02b61d15a9b1ada
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-01 23:48:05 -0300

    Pasar parametros sin arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import React, { Component } from 'react'
 
 class App extends Component {
 
-    handleClick = (e, msg) => {
+    handleClick(msg, msg2, e) {
+        console.log(e)
         console.log(msg)
-        console.log(e.target)
+        console.log(msg2)
     }
 
     render() {
         return (
             <>
                 <h1>Events</h1>
-                <button onClick={(e) => this.handleClick(e, 'Hello')}>Click me!</button>
+                <button onClick={this.handleClick.bind(this, 'hello', 'world')}>Click me!</button>
             </>
         )
     }
@@ -22,34 +23,19 @@ export default App;
 
 
 /*
-Paso de parametros con arrow function:
+Paso de parametros sin arrow function:
 
-Como enviar parametros cuando ejecutamos un evento (con arrow function)
+<button onClick={this.handleClick.bind(this, 'hello', 'world')}>Click me!</button>
 
-Cuando ejecutamos un evento de esta forma automaticamente y sin que podamos evitarlo se va a enviar el evento.
-Podemos no recogerlo pero el evento se esta enviando.
+La forma de pasar parametros si no contamos con arrow functions es utilizar el metodo 'bind' y pasar como primer parametro 'this', luego podemos pasar uno o mas parametros.
 
-<button onClick={this.handleClick}>Click me!</button>
 
-Si queremos pasar mas parametros u otro parametro en lugar del evento (e) entonces necesitamos que la funcion sea llamda de la siguiente manera:
-
-handleClick = (msg) => {
+handleClick(msg, msg2, e) {
+    console.log(e)
     console.log(msg)
+    console.log(msg2)
 }
 
-<button onClick={() => this.handleClick(e, 'Hello')}>Click me!</button>
-
-
-Si ademas necesitamos pasar el evento:
-
-handleClick = (e, msg) => {
-    console.log(msg)
-    console.log(e.target)
-}
-
-<button onClick={(e) => this.handleClick(e, 'Hello')}>Click me!</button>
-
-'e' es el evento pero se esta ejecutando a traves de una funcion anonima por lo tanto el parametro de evento lo tiene que recoger la funcion que esta ejecutando el evento.
-
+En el 'handle' debemos pasar los parametros pero siempre el evento (e) debe ser el ultimo.
 
 */
